33b1246353918ad6403467335f0299673af4f450
Istiak Ahmed <istiakahmed6219@gmail.com>
2024-09-26 19:40:17 +0600

    basics signup

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const { signUp, signIn, logOut } = require("../controller/user");
+const userModel = require("../models/user.model");
 
 const router = express.Router();
 
-router.post("/signup", signUp);
+router.post("/signup", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  await userModel.create({
+    username: username,
+    email: email,
+    password: password,
+  });
+
+  res.json({
+    message: "User created successfully",
+  });
+});
 router.post("/signin", signIn);
 router.post("/logout", logOut);
 
-router.module.exports = router;
+module.exports = router;
